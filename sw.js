b4382109421b3270fbfba5efa4a0a2fe4a4fc6e8
dataweb76-sys <TU_EMAIL_DE_GITHUB>
2026-05-28 10:32:38 -0300

    Bumps SW cache a tc-v4

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    - Manejo de Push Notifications
 ══════════════════════════════════════════════ */
 
-const CACHE_NAME = 'tc-v3'
+const CACHE_NAME = 'tc-v4'
 const CACHE_STATIC = ['/index.html', '/style.css', '/logo.png']
 
 /* ── Instalación ── */
